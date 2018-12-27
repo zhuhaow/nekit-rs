736c179a0e05ba0d9d0d0ffa6410ae83f1bacce7
zhuhaow <zhuhaow@gmail.com>
2018-12-27 20:11:52 +0800

    Add `Connector` and `TcpConnector`

---
+++ src/connector/tcp_connector.rs
@@ -0,0 +1,27 @@
+use connector::Connector;
+use std::io::Error;
+use tokio::{net::TcpStream, prelude::*};
+use utils::{Endpoint, Resolver};
+
+pub struct TcpConnector {
+    resolver: Box<Resolver>,
+}
+
+impl TcpConnector {
+    pub fn new(resolver: Box<Resolver>) -> Self {
+        TcpConnector { resolver }
+    }
+}
+
+impl Connector<TcpStream> for TcpConnector {
+    fn connect(
+        mut self,
+        endpoint: &Endpoint,
+    ) -> Box<Future<Item = TcpStream, Error = Error> + Send> {
+        Box::new(
+            self.resolver
+                .resolve_endpoint(endpoint)
+                .and_then(|addrs| TcpStream::connect(addrs.first().unwrap())),
+        )
+    }
+}
