ddee277f0e2cf6a8da60e11023edf0cb2ccaa2c0
zhuhaow <zhuhaow@gmail.com>
2019-07-09 22:18:35 +0800

    Fix recusive overflow issue on Windows

--- specht2/src/bin/specht.rs
+++ specht2/src/bin/specht.rs
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#![recursion_limit = "128"]
+
 use freighter::acceptor::http::HttpAcceptor;
 use freighter::acceptor::http::HttpProxyTransformerBuilder;
 use freighter::connector::Connector;
